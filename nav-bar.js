c42ef5c0bc564a79e728c49ef0f80b1aa5c9413d
zainabmeerza <zainabmeerza@gmail.com>
2021-07-31 16:08:40 +0000

    Align the images in the gallery to the center

---
+++ nav-bar.js
@@ -0,0 +1,11 @@
+/**
+ *  Part of responsive navbar code found on W3 schools
+ */
+ function navFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
